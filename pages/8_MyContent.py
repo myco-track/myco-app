16d03e398fe60137ae209d59e5ffffa31ebd88ef
Sibila <ss2190@hw.ac.uk>
2024-06-08 01:18:03 +0100

    navbar added to mycontent

--- pages/8_MyContent.py
+++ pages/8_MyContent.py
@@ -1,5 +1,88 @@
 import streamlit as st
 
+#navbar
+# Custom CSS for the navigation bar
+st.markdown(
+    """
+    <style>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: rgb(46 56 70/var(--tw-bg-opacity));
+        padding: 10px 20px;
+        border-radius: 8px;
+    }
+    .navbar img {
+        height: 40px;
+    }
+    .navbar .nav-links {
+        display: flex;
+        align-items: center;
+    }
+    .navbar .nav-links a, .navbar .nav-links button {
+        color: rgb(46 56 70/var(--tw-bg-opacity));
+        margin-left: 20px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 18px;
+        font-family: Arial, sans-serif;
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+    .navbar .nav-links a:hover, .navbar .nav-links button :hover {
+        background-color: rgb(46 56 70/var(--tw-bg-opacity));
+        color: black;
+        border-radius: 4px;
+    }
+    .upload-btn-wrapper {
+        position: relative;
+        overflow: hidden;
+        display: inline-block;
+        cursor: pointer;
+    }
+    .upload-btn-wrapper:hover {
+        cursor: pointer;
+    }
+    .upload-btn-wrapper button {
+        border: 2px solid #4CAF50;
+        color: white;
+        background-color: #4CAF50;
+        padding: 8px 20px;
+        border-radius: 8px;
+        font-size: 16px;
+        font-weight: bold;
+         cursor: pointer;
+    }
+    .upload-btn-wrapper input[type=file] {
+        font-size: 100px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        opacity: 0;
+         cursor: pointer;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Navbar HTML
+st.markdown("""
+    <div class="navbar">
+        <img src="https://about.myco.io/wp-content/uploads/2022/11/myco-logo-variation-4-500px-300x124.png" alt="Logo" width="100" height="100">
+        <div class="nav-links">
+            <a href="#search"><img src="https://img.icons8.com/ios-filled/50/ffffff/search.png" ></a>
+            <div class="upload-btn-wrapper" id="upload-btn-wrapper">
+                <button onclick="showPopup()">Upload</button>
+                <input type="file" name="upload"/>
+            </div>
+            <button><img src="https://img.icons8.com/ios-filled/50/ffffff/user.png" height="24"></button>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
 # Title
 st.title("Profile")
 
